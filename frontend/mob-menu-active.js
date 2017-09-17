83f65a01694eeacc52105f8ed7a7837262896286
Ekaterina Turalnikova <turalnikova@gmail.com>
2017-09-17 14:20:53 +0300

    first commit

---
+++ frontend/mob-menu-active.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Администратор on 22.05.2017.
+ */
+
+$(document).ready(function(){
+    $('.mob-menu-icon').click(function(){
+        $('.header__nav').css('display','flex');
+    });
+    $('.mob-menu-icon').dbclick(function(){
+        $('.header__nav').css('display','none');
+    });
+});
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtb2ItbWVudS1hY3RpdmUuanMiXSwic291cmNlc0NvbnRlbnQiOlsiLyoqXHJcbiAqIENyZWF0ZWQgYnkg0JDQtNC80LjQvdC40YHRgtGA0LDRgtC+0YAgb24gMjIuMDUuMjAxNy5cclxuICovXHJcbjxzY3JpcHQ+XHJcbiQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uKCl7XHJcbiAgICAkKCcubW9iLW1lbnUtaWNvbicpLmNsaWNrKGZ1bmN0aW9uKCl7XHJcbiAgICAgICAgJCgnLmhlYWRlcl9fbmF2JykuY3NzKCdkaXNwbGF5JywnZmxleCcpO1xyXG4gICAgfSk7XHJcbiAgICAkKCcubW9iLW1lbnUtaWNvbicpLmRiY2xpY2soZnVuY3Rpb24oKXtcclxuICAgICAgICAkKCcuaGVhZGVyX19uYXYnKS5jc3MoJ2Rpc3BsYXknLCdub25lJyk7XHJcbiAgICB9KTtcclxufSk7XHJcbjwvc2NyaXB0PiJdLCJmaWxlIjoibW9iLW1lbnUtYWN0aXZlLmpzIn0=
